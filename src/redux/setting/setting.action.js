0a7c0b9fec6bee0fdbbb1be2db64ea7e3b5a577d
evyatarhaim1 <evyataros1@gmail.com>
2024-06-29 00:20:46 +0300

    Updates

--- src/redux/setting/setting.action.js
+++
@@ -1,6 +0,0 @@
-export const SWITCH_TO_DARKMODE = 'SWITCH_TO_DARKMODE';
-export const FILTER_BY_PROJECT_TYPE = 'FILTER_BY_PROJECT_TYPE';
-
-export const switchToDarkmode = (darkMode) => { return { type: SWITCH_TO_DARKMODE, payload: darkMode } }
-
-export const filterByProjectType = (types) => { return { type: FILTER_BY_PROJECT_TYPE, payload: types } }
